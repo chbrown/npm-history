34e1f5834ab74cabf7aac5cf082623b5de622210
Christopher Brown <io@henrian.com>
2015-06-18 14:59:30 -0500

    Import regex-router built-in d.ts in main type declarations index

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -3,3 +3,5 @@
 /// <reference path="DefinitelyTyped/node/node.d.ts" />
 /// <reference path="DefinitelyTyped/request/request.d.ts" />
 /// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
+
+/// <reference path="../node_modules/regex-router/regex-router.d.ts" />
